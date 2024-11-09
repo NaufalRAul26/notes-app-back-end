7a3c1592fefd5ae76d785756ca74dfdab3437ff6
Naufal <naufalrifqiazfar@gmail.com>
2024-11-09 23:11:56 +0700

    initial commit

---
+++ routes.js
@@ -0,0 +1,36 @@
+// Mengimpor fungsi addNoteHandler dari berkas handler.js
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+
+// Konfigurasi route
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler, // Menambahkan handler untuk route ini
+    
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
+
---
+++ server.js
@@ -0,0 +1,24 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes'); // Mengimpor konfigurasi routes
+
+const init = async () => {
+  // Membuat instance server
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // Menambahkan konfigurasi route ke dalam server
+  server.route(routes); // Menggunakan route yang sudah didefinisikan
+
+  // Memulai server
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`); // Menampilkan URI server setelah server berjalan
+};
+
+init(); // Memanggil fungsi init untuk menjalankan server
