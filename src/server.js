05f38be94ad5a323076d635fba74da859687e77b
Naufal <naufalrifqiazfar@gmail.com>
2024-11-10 23:33:03 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,24 +1,21 @@
 const Hapi = require("@hapi/hapi");
-const routes = require("./routes"); // Mengimpor konfigurasi routes
+const routes = require("./routes.js");
 
 const init = async () => {
-  // Membuat instance server
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
 
-  // Menambahkan konfigurasi route ke dalam server
-  server.route(routes); // Menggunakan route yang sudah didefinisikan
+  server.route(routes);
 
-  // Memulai server
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`); // Menampilkan URI server setelah server berjalan
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-init(); // Memanggil fungsi init untuk menjalankan server
+init();
